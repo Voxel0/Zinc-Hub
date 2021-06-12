ddb018eb0400f50a6db55dfacc77b69b05648f5a
Voxel <80284601+Voxel0@users.noreply.github.com>
2021-06-11 18:58:19 -0600

    Update Main.lua

--- Main.lua
+++ Main.lua
@@ -2,7 +2,7 @@ local HttpService = game:GetService('HttpService')
 local Supported = false
 local Script = nil
 
-local GameList = game:HttpGet('https://raw.githubusercontent.com/Voxel0/Zinc-Hub/main/GameList.json')
+local GameList = game:HttpGet('https://raw.githubusercontent.com/Voxel0/ZincHub/main/GameList.json')
 function GetGame()
     local GameTable = HttpService:JSONDecode(GameList)
     if GameTable[tostring(game.PlaceId)] then 
